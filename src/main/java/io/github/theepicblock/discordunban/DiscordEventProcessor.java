b16650b773fdf14d309aa78d5c5ebc96227f40c6
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-05-24 16:42:04 +0200

    Reload when discordSRV reloads

--- src/main/java/io/github/theepicblock/discordunban/DiscordEventProcessor.java
+++ src/main/java/io/github/theepicblock/discordunban/DiscordEventProcessor.java
@@ -2,6 +2,7 @@ package io.github.theepicblock.discordunban;
 
 import github.scarsz.discordsrv.api.ListenerPriority;
 import github.scarsz.discordsrv.api.Subscribe;
+import github.scarsz.discordsrv.api.events.ConfigReloadedEvent;
 import github.scarsz.discordsrv.api.events.DiscordGuildMessageReceivedEvent;
 import github.scarsz.discordsrv.dependencies.jda.api.events.message.react.MessageReactionAddEvent;
 import github.scarsz.discordsrv.dependencies.jda.api.hooks.ListenerAdapter;
@@ -13,14 +14,25 @@ import javax.annotation.Nonnull;
  */
 public class DiscordEventProcessor {
     private MessageProcessor messageProcessor;
+    private final DiscordUnban plugin;
 
-    public DiscordEventProcessor(MessageProcessor messageProcessor) {
+    public DiscordEventProcessor(MessageProcessor messageProcessor, DiscordUnban plugin) {
+        this.messageProcessor = messageProcessor;
+        this.plugin = plugin;
+    }
+
+    public void setMessageProcessor(MessageProcessor messageProcessor) {
         this.messageProcessor = messageProcessor;
     }
 
     @Subscribe(priority = ListenerPriority.MONITOR)
     public void discordMessageReceived(DiscordGuildMessageReceivedEvent event) {
         messageProcessor.process(event.getMessage());
+    }
+
+    @Subscribe(priority = ListenerPriority.MONITOR)
+    public void configReloaded(ConfigReloadedEvent event) {
+        plugin.reload();
     }
 
     public static class JDAReactionListener extends ListenerAdapter {
